7b0188ab21cfabb73542300947c87ce914b9b096
irma660 <1325455@senati.pe>
2023-08-22 18:50:08 -0500

    ejemplo de recursividad: creando función sumar

--- EjemploRecursividad.java
+++ EjemploRecursividad.java
@@ -1,7 +1,14 @@
 package ejemplorecursividad;
 /**@author Irma*/
 public class EjemploRecursividad {
-    public static void main(String[] args) {
+    public static int sumaN (int numero){
+        if ( numero == 1){
+            return 1;
+        }else {
+        return numero + sumaN(numero - 1);
+        }
+}
+    public static void main(String[] args) { 
     }
     
 }
