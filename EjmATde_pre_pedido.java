c1a6a53710e6b2b9145597aab939c90331830711
irma660 <1325455@senati.pe>
2023-08-22 20:32:47 -0500

    creando la función de recomendar productos, y agregué una lista de productos relacionados

--- EjmATde_pre_pedido.java
+++ EjmATde_pre_pedido.java
@@ -10,6 +10,12 @@ class producto{
     }
 }
 public class EjmATde_pre_pedido {
+    public static List<producto> recomendacion(List<producto>productosComprados){
+        List<producto> recomendaciones = new ArrayList<>();
+        recomendaciones.add(new producto("café"));
+        recomendaciones.add(new producto("azúcar"));
+        return recomendaciones;
+    }
     public static void main(String[] args){ 
     }
     
