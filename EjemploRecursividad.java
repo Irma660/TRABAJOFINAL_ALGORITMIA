d1f41b0a4f963be2f30984b9a03ff4f881da7921
irma660 <1325455@senati.pe>
2023-08-23 00:08:15 -0500

    importando paquetes a usarse

--- EjemploRecursividad.java
+++
@@ -1,7 +0,0 @@
-package ejemplorecursividad;
-/**@author Irma*/
-public class EjemploRecursividad {
-    public static void main(String[] args) {
-    }
-    
-}
--- EjmAT_post_pedido.java
+++ EjmAT_post_pedido.java
@@ -1,5 +1,8 @@
 package ejmat_post_pedido;
 /* @author Irma */
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 public class EjmAT_post_pedido {
     public static void main(String[] args) {
       //EJERCICIO DE ASIGNACIÓN DE PEDIDOS
