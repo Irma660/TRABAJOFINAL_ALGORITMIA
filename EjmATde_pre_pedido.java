728ad695d19316de399d9f4cb2e30a503babcaa8
irma660 <1325455@senati.pe>
2023-08-22 19:58:55 -0500

    ejemplo algoritmo transversal-pre pedido

--- EjmATde_pre_pedido.java
+++ EjmATde_pre_pedido.java
@@ -1,7 +1,7 @@
 package ejmatde_pre_pedido;
 /*@author Irma*/
 public class EjmATde_pre_pedido {
-    public static void main(String[] args){
+    public static void main(String[] args){ 
     }
     
 }
