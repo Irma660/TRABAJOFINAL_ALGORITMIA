b2f84b5634c0d38cdedc51bcd9ff5d2bf7a4c0ad
irma660 <1325455@senati.pe>
2023-08-22 20:17:14 -0500

    creando clase producto

--- EjmATde_pre_pedido.java
+++ EjmATde_pre_pedido.java
@@ -2,6 +2,13 @@ package ejmatde_pre_pedido;
 /*@author Irma*/
 import java.util.ArrayList;
 import java.util.List;
+class producto{
+    String nombre;
+    
+    public producto(String nombre) {
+        this.nombre = nombre;
+    }
+}
 public class EjmATde_pre_pedido {
     public static void main(String[] args){ 
     }
