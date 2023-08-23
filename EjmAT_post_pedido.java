ec5067e4f3fbcbded945efe6139aef255293a3a8
irma660 <1325455@senati.pe>
2023-08-23 00:40:42 -0500

    declaramos Scanner, añandimos la clase almacen y su método

--- EjmAT_post_pedido.java
+++ EjmAT_post_pedido.java
@@ -6,6 +6,23 @@ import java.util.Scanner;
 public class EjmAT_post_pedido {
     public static void main(String[] args) {
       //EJERCICIO DE ASIGNACIÓN DE PEDIDOS
+      Scanner leer = new Scanner(System.in);
+      //CREANDO CLASE ALMACEN
+      class Almacen {
+          private String nombre;
+          private String dirección;
+          //la distancia es real
+          private int distancia;
+          public Almacen(String nombre, String direccion,int capacidad){
+              this.nombre = nombre;
+              this.dirección = dirección;
+              this.distancia = distancia;
+          }
+          public String getNombre(){
+              return nombre;
+          }
+      }
+      
     }
     
 }
