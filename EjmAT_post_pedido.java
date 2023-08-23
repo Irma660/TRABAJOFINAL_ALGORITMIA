984c41788acf3568e7d3ade8942df3ebc1d1b367
irma660 <1325455@senati.pe>
2023-08-22 23:57:21 -0500

    ejercicio de algoritmo transversal post-pedido

---
+++ EjmAT_post_pedido.java
@@ -0,0 +1,8 @@
+package ejmat_post_pedido;
+/* @author Irma */
+public class EjmAT_post_pedido {
+    public static void main(String[] args) {
+      //EJERCICIO DE ASIGNACIÓN DE PEDIDOS
+    }
+    
+}
