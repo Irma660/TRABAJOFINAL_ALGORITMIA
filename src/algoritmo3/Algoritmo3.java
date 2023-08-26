8df2532ba736b79d5edcb3d14d3c45cec71789f5
irma660 <1325455@senati.pe>
2023-08-26 18:37:06 -0500

    declarando variable e importante la scanner

--- src/algoritmo3/Algoritmo3.java
+++ src/algoritmo3/Algoritmo3.java
@@ -1,6 +1,11 @@
 package algoritmo3;
 /* @author Irma */
+import java.util.Scanner;
 public class Algoritmo3 {
     public static void main(String[] args) {
+        //algoritmo de verificar si el número es positivo o negativo
+        double num;
+        Scanner entrada = new Scanner(System.in);
+        
     }  
 }
