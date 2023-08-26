9cc163bca14d01d5a77c7ec4baadf03a2ba9f892
irma660 <1325455@senati.pe>
2023-08-26 16:36:00 -0500

    declarando variable, importando objeto en Scanner

--- src/algoritmo2/Algoritmo2.java
+++ src/algoritmo2/Algoritmo2.java
@@ -1,6 +1,9 @@
 package algoritmo2;
 /* @author Irma*/
+import java.util.Scanner;
 public class Algoritmo2 {
     public static void main(String[] args) {
+        double puntaje;
+        Scanner entrada = new Scanner(System.in);
     }  
 }
