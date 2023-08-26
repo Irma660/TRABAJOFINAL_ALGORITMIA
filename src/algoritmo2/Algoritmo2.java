783bcca66bdf4f6bf6c3608eb4d0788f883216d5
irma660 <1325455@senati.pe>
2023-08-26 16:38:54 -0500

    pidiendo ingresar puntaje

--- src/algoritmo2/Algoritmo2.java
+++ src/algoritmo2/Algoritmo2.java
@@ -5,5 +5,9 @@ public class Algoritmo2 {
     public static void main(String[] args) {
         double puntaje;
         Scanner entrada = new Scanner(System.in);
+        //ingresar puntaje
+        System.out.println("=== EVALUACIÓN DE EXAMEN ===");
+        System.out.println("INGRESAR PUNTACIÓN DEL ESTUDIANTE: ");
+        puntaje = entrada.nextDouble();
     }  
 }
