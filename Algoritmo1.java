15eb0ba7fb1555c52c4f897b0de7a762c96f0e1c
irma660 <1325455@senati.pe>
2023-08-26 13:13:33 -0500

    declarando variable y solicitar el ingreso de un número entero

--- Algoritmo1.java
+++ Algoritmo1.java
@@ -1,8 +1,16 @@
 package algoritmo1;
 /* @author Irma */
+import java.util.Scanner;
 public class Algoritmo1 {
     public static void main(String[] args) {
        /*EJEMPLO DE ALGORITMO 01*/
+       int número;
+       Scanner entrada = new Scanner(System.in);
+       
+       //solicitar al usuario ingresar un número entero
+        System.out.println("=== CALCULAR FACTORIAL ===");
+        System.out.println("INGRESE UN NÚMERO ENTERO: ");
+        número = entrada.nextInt();
     }
     
 }
