7cb9eb082006054c5e2c96ea998fc31a744a5442
irma660 <1325455@senati.pe>
2023-08-26 23:08:23 -0500

    verificar si tiene licencia vigente

--- src/algoritmo5/Algoritmo5.java
+++ src/algoritmo5/Algoritmo5.java
@@ -3,14 +3,20 @@ package algoritmo5;
 import java.util.Scanner;
 public class Algoritmo5 {
     public static void main(String[] args) {
-        // algoritmo5 
+        // algoritmo5, validación de licencias de conducir 
         int edad;
         Scanner leer = new Scanner(System.in);
         //pidiendo ingresar edad
         System.out.println("---VERIFICACIÓN DE LICENCIAS DE CONDUCIR---");
         System.out.println("->INGRESA TU EDAD: ");
         edad = leer.nextInt();
-            
+         //verificando 
+         if (edad >= 18){
+             System.out.println("¿TIENES LICENCIA DE CONDUCIR, VIGENTE?");
+             System.out.println("---->(SI/NO)");
+             //aceptar palabras en mayuscula o minuscula
+             String tieneL = leer.next().toLowerCase();
+         }
         
     }
 }
