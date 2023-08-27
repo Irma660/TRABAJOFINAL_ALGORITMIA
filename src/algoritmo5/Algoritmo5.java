978946ec19dcba85060c7c59b49be55931020411
irma660 <1325455@senati.pe>
2023-08-26 22:59:19 -0500

    ingresar edad

--- src/algoritmo5/Algoritmo5.java
+++ src/algoritmo5/Algoritmo5.java
@@ -6,5 +6,11 @@ public class Algoritmo5 {
         // algoritmo5 
         int edad;
         Scanner leer = new Scanner(System.in);
+        //pidiendo ingresar edad
+        System.out.println("---VERIFICACIÓN DE LICENCIAS DE CONDUCIR---");
+        System.out.println("->INGRESA TU EDAD: ");
+        edad = leer.nextInt();
+            
+        
     }
 }
