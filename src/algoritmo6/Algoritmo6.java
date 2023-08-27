411f1fd2a6b67abea7ba37dc111a917296187104
irma660 <1325455@senati.pe>
2023-08-27 00:31:01 -0500

    declarando variable y creando objeto de entrada

--- src/algoritmo6/Algoritmo6.java
+++ src/algoritmo6/Algoritmo6.java
@@ -1,8 +1,11 @@
 package algoritmo6;
 /*@author Irma*/
+import java.util.Scanner;
 public class Algoritmo6 {
     public static void main(String[] args) {
-        // algoritmo 6
-    }
-    
+        // algoritmo 6, sistema de decisión de llevar paraguas
+        String lluvia;
+        Scanner scanner = new Scanner(System.in);
+        
+    }    
 }
