e87126b782efa4bfe0cfa28f95f9300bb1a6dce7
irma660 <1325455@senati.pe>
2023-08-27 09:31:05 -0500

    añadiendo switch

---
+++ Algoritmo6.java
@@ -0,0 +1,23 @@
+package algoritmo6;
+/*@author Irma*/
+import java.util.Scanner;
+public class Algoritmo6 {
+    public static void main(String[] args) {
+        // algoritmo 6, sistema de decisión de llevar paraguas
+        String lluvia;
+        
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("====DECISIÓN DE LLEVAR PARAGUAS====");
+        System.out.println("---------¿está lloviendo?----------");
+        System.out.println("-------> SI/NO          -------    ");
+        //aceptar minuscula o mayuscula
+        lluvia = scanner.next().toLowerCase();
+        //swicth
+        switch (lluvia) {
+           case "si":
+           System.out.println("        ¿Tienes paraguas?          ");
+           System.out.println("-------> SI/NO          -------    ");
+           String paraguas = scanner.next().toLowerCase();
+        }
+    }    
+}
