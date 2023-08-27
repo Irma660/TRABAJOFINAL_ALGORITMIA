2a6a0bd90a90df54077d3017fa1c50dfce27c588
irma660 <1325455@senati.pe>
2023-08-26 20:22:57 -0500

    agregando excepción

--- src/algoritmo4/Algoritmo4.java
+++ src/algoritmo4/Algoritmo4.java
@@ -8,9 +8,14 @@ public class Algoritmo4 {
         String frase = JOptionPane.showInputDialog("INGRESAR FRASE");
         int contador = 0;
         for (int i = 0; i < frase.length(); i++) {
+            char caracter = frase.charAt(i);
+            if (Character.isLetter(caracter)){
             if (frase.charAt(i) == 'a' || frase.charAt(i) == 'A') {
             contador++;
-        }
+            }
+            }else {
+                JOptionPane.showConfirmDialog(null, "ERROR. INGRESÓ CARACTERES NO VÁLIDOS");
+            }
         }
         JOptionPane.showConfirmDialog(null, "La frase \"" + frase + "\" contiene " + contador + " letras 'a'.");
     }  
