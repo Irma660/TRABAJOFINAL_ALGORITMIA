49d7dbb212ec7ca7d8fbbe4f647026f35303df09
irma660 <1325455@senati.pe>
2023-08-26 20:02:05 -0500

    agregando contador

--- src/algoritmo4/Algoritmo4.java
+++ src/algoritmo4/Algoritmo4.java
@@ -3,8 +3,14 @@ package algoritmo4;
 import javax.swing.JOptionPane;
 public class Algoritmo4 {
     public static void main(String[] args) {
-        //ejercicio contador de letras con joptionPane
-        int contador = 0;
+        //ejercicio contador de letras a con joptionPane
+        
         String frase = JOptionPane.showInputDialog("INGRESAR FRASE");
+        int contador = 0;
+        for (int i = 0; i < frase.length(); i++) {
+            if (frase.charAt(i) == 'a' || frase.charAt(i) == 'A') {
+            contador++;
+        }
+        }
     }  
 }
