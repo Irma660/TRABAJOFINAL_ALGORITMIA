d11b783b944fae66e01ec4b005762b129c1e021d
irma660 <1325455@senati.pe>
2023-08-26 22:00:48 -0500

    creando objeto de scanner

--- src/algoritmo5/Algoritmo5.java
+++ src/algoritmo5/Algoritmo5.java
@@ -1,7 +1,9 @@
 package algoritmo5;
 /* @author Irma*/
+import java.util.Scanner;
 public class Algoritmo5 {
     public static void main(String[] args) {
         // algoritmo5 
+        Scanner leer = new Scanner(System.in);
     }
 }
