bdf8beb07dfa5241a010686134c6adcc91118b60
irma660 <1325455@senati.pe>
2023-08-26 19:52:54 -0500

    importando JOptionPane y declarando variables

--- src/algoritmo4/Algoritmo4.java
+++ src/algoritmo4/Algoritmo4.java
@@ -1,6 +1,10 @@
 package algoritmo4;
 /* @author Irma */
+import javax.swing.JOptionPane;
 public class Algoritmo4 {
     public static void main(String[] args) {
+        //ejercicio contador de letras con joptionPane
+        int contador = 0;
+        String frase = JOptionPane.showInputDialog("INGRESAR FRASE");
     }  
 }
